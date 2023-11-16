c43a18ae34bc3505d2bf98eb8403d052ab54cc75
Benjamin Ohene Konadu <benohenekonadu@outlook.com>
2023-11-16 15:51:08 +0100

    Added the service section

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,18 @@ toggleButton.addEventListener('click', (e) => {
         navbarLinks.classList.remove('active');
     });
 });
+
+// Qualification section
+let tablinks = document.getElementsByClassName("qualify-links");
+let tabcontents = document.getElementsByClassName("qualify-contents");
+
+function opentab(tabname) {
+    for(tablink of tablinks) {
+        tablink.classList.remove("active");
+    }
+    for(tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active");
+    document.getElementById(tabname).classList.add("active-tab");
+}
