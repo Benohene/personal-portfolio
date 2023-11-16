e820c27b825b29c0b271a4a39a140d36781b55e4
Benjamin Ohene Konadu <benohenekonadu@outlook.com>
2023-11-16 11:37:19 +0100

    Add hamburger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-function toggleMenu() {
-    var navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('show');
-}
+// Hamburger menu
+const toggleButton = document.querySelector('.toggleBtn');
+const navbarLinks = document.querySelector('.nav-links');
+
+toggleButton.addEventListener('click', (e) => {
+  e.preventDefault(); // Prevents the default behavior of the anchor element
+  navbarLinks.classList.toggle('active');
+//   remove nav links after clicking a link
+    navbarLinks.addEventListener('click', (e) => {
+        navbarLinks.classList.remove('active');
+    });
+});
